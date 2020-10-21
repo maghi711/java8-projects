b412e9bd946dd58bb5aefad45b80cffe43234e28
Mahesh J <maghi711@gmail.com>
2020-10-21 18:03:55 +0530

    Place 0 and 1 adjacent without having two adjacent 1's same as A B problem discussed.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

--- src/main/java/com/aadavan/check/dsa/dp/counting/AdjacentBinarysOrABProblem.java
+++ src/main/java/com/aadavan/check/dsa/dp/counting/AdjacentBinarysOrABProblem.java
@@ -2,7 +2,7 @@ package com.aadavan.check.dsa.dp.counting;
 
 import java.util.Scanner;
 
-public class TwoCrossNProblemSecondApproach {
+public class AdjacentBinarysOrABProblem {
     static int MOD = (int)1e9+7;
     static long sumMOD(long a, long b){
         return (a%MOD + b%MOD)%MOD;
@@ -12,8 +12,8 @@ public class TwoCrossNProblemSecondApproach {
     public static void main(String[] args)
     {
         int t = sc.nextInt();
-        dp[1] = 1l;
-        dp[2] = 2l;
+        dp[1] = 2l;
+        dp[2] = 3l;
         precompute();
         while(t-- != 0) {
             int n = sc.nextInt();
@@ -22,17 +22,8 @@ public class TwoCrossNProblemSecondApproach {
     }
 
     static void precompute() {
-        for (int i = 3; i <dp.length; i++) {
+        for (int i = 3; i <=dp.length; i++) {
             dp[i] = sumMOD(dp[i-1],dp[i-2]);
         }
     }
-
-    static long twoCrossTwo(int n) {
-        if (n < 3) return dp[n];
-        if (dp[n] == 0) {
-            dp[n] = sumMOD(dp[n-1], dp[n-2]);
-        }
-        return dp[n];
-    }
-
 }
