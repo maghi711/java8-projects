c23dbdda96d653577e4cd77eb2044ba6ad54f11b
Mahesh J <maghi711@gmail.com>
2020-10-22 06:56:12 +0530

    Climbing to reach stairs top solution.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

---
+++ src/main/java/com/aadavan/check/dsa/dp/counting/ClimginStairsToReachTop.java
@@ -0,0 +1,36 @@
+package com.aadavan.check.dsa.dp.counting;
+
+import java.util.Scanner;
+
+public class ClimginStairsToReachTop {
+    int MOD = (int)1e9+7;
+    long sumMOD(long a, long b){
+        return (a%MOD + b%MOD)%MOD;
+    }
+    long prodMOD(long a, long b){
+        return (a%MOD * b%MOD)%MOD;
+    }
+    long dp[] = new long[101];
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        ClimginStairsToReachTop t2 = new ClimginStairsToReachTop();
+        t2.precompute();
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            System.out.println(t2.solution(n));
+        }
+    }
+    void precompute() {
+        if (dp[1] == 0) {
+            dp[0] = 0l;
+            dp[1] = 1l;
+            dp[2] = 2l;
+            for (int i = 3; i <dp.length; i++) {
+                dp[i] = sumMOD(dp[i - 1], dp[i - 2]);
+            }
+        }
+    }
+    long solution(int n) {
+        return dp[n];
+    }
+}
