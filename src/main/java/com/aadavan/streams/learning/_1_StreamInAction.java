94a71734700713b227384b812ecbf0cb82a34ffb
Mahesh J <maghi711@gmail.com>
2020-10-24 12:10:01 +0530

    Convert a list into a stream and print them and handle exception.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

--- src/main/java/com/aadavan/streams/learning/_1_StreamInAction.java
+++ src/main/java/com/aadavan/streams/learning/_1_StreamInAction.java
@@ -15,17 +15,27 @@ public class _1_StreamInAction {
     private static void createStreamFromAnArray() {
         String[] words = {"hello", "hola", "hallo", "ciao"};
         Stream<String> stream = Stream.of(words);
-        System.out.println("stream = " + stream);
+        stream.forEach(x -> System.out.print(x + " "));
+        System.out.println();
     }
 
     private static void createStreamFromStrings() {
         final Stream<String> stream = Stream.of("hello", "hola", "hallo", "ciao");
-        System.out.println("stream = " + stream);
+        stream.forEach(x -> System.out.print(x + " "));
+        System.out.println();
     }
 
     private static void createStreamFromAList() {
-        List<String> words = Arrays.asList("Aadavan", "Swetha", "Mahesh");
+        List<String> words = Arrays.asList("Aadavan", null, "Mahesh");
         final Stream<String> stream = words.stream();
-        System.out.println("stream = " + stream);
+        stream.forEach(x -> {
+            try {
+                System.out.print(x.toString());
+            } catch (Exception e) {
+                System.out.println("\nException occurred " + e.getMessage());
+            }
+        }
+        );
+        System.out.println();
     }
 }
