b911bc6935079f14cebbcf30db3e53afe90216fe
Mahesh J <maghi711@gmail.com>
2020-10-25 08:26:31 +0530

    Range sum contest solution
    Signed-off-by: Mahesh J <maghi711@gmail.com>

---
+++ src/main/java/com/aadavan/dsa/dp/contest2/RangeSum.java
@@ -0,0 +1,30 @@
+package com.aadavan.dsa.dp.contest2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RangeSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- != 0) {
+            int size = s.nextInt();
+            int a[] = new int[size];
+            for (int i = 0; i < size; i++) {
+                a[i] = s.nextInt();
+            }
+            int t2 = s.nextInt();
+            while(t2-- != 0) {
+                int start = s.nextInt();
+                int end = s.nextInt();
+                int sum = 0;
+                for (int i = start; i <= end; i++) {
+                    sum += a[i];
+                }
+                System.out.println(sum);
+            }
+            System.out.println("Arrays.toString(a) = " + Arrays.toString(a));
+        }
+        s.close();
+    }
+}
