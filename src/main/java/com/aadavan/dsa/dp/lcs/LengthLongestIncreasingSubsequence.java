a9d6dbeec89f02ea85c0ab9af77aa0a90fc4778e
Mahesh J <maghi711@gmail.com>
2020-10-26 16:00:01 +0530

    Longest length increasing subsequence with duplicates removal.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

---
+++ src/main/java/com/aadavan/dsa/dp/lcs/LengthLongestIncreasingSubsequence.java
@@ -0,0 +1,61 @@
+package com.aadavan.dsa.dp.lcs;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class LengthLongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- != 0) {
+            int length = scanner.nextInt();
+            int a[] = new int[length];
+            for (int i = 0; i < length; i++) {
+                a[i] = scanner.nextInt();
+            }
+            System.out.println(longestLISLength(a));
+        }
+    }
+
+    static int[] b;
+    static int[][] dp = new int[1001][1001];
+    private static int longestLISLength(int[] a) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int v: a) {
+            set.add(v);
+        }
+        b = new int[set.size()];
+        Iterator<Integer> value = set.iterator();
+        int val = 0;
+        while (value.hasNext()) {
+            Integer next = value.next();
+            b[val++] = next;
+        }
+        Arrays.sort(b);
+        System.out.println("Arrays.toString(b) = " + Arrays.toString(b));
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j <= b.length; j++) {
+                if (a[i-1] == b[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        printDpArray(a.length, b.length);
+        return dp[a.length][b.length];
+    }
+
+    private static void printDpArray(int aLength, int bLength) {
+        // Print the matrix to check
+        for (int i = 0; i <= bLength; i++) {
+            for (int j = 0; j <= aLength; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
