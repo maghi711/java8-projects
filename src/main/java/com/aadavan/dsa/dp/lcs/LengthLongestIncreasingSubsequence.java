a181da4afc2ea0f282eb772b8cac434958ecc9da
Mahesh J <maghi711@gmail.com>
2020-10-26 16:12:15 +0530

    Length of Longest common increasing subsequence
    Signed-off-by: Mahesh J <maghi711@gmail.com>

--- src/main/java/com/aadavan/dsa/dp/lcs/LengthLongestIncreasingSubsequence.java
+++ src/main/java/com/aadavan/dsa/dp/lcs/LengthLongestIncreasingSubsequence.java
@@ -59,3 +59,32 @@ public class LengthLongestIncreasingSubsequence {
         System.out.println();
     }
 }
+/*
+
+int longestLISLength(int a[]) {
+  	int[][] dp = new int[1001][1001];
+    HashSet<Integer> set = new HashSet<>();
+    for(int v: a) {
+      set.add(v);
+    }
+    int[] b = b = new int[set.size()];
+    Iterator<Integer> value = set.iterator();
+    int val = 0;
+    while (value.hasNext()) {
+      Integer next = value.next();
+      b[val++] = next;
+    }
+    Arrays.sort(b);
+
+    for (int i = 1; i <= a.length; i++) {
+      for (int j = 1; j <= b.length; j++) {
+        if (a[i-1] == b[j-1]) {
+          dp[i][j] = 1 + dp[i-1][j-1];
+        } else {
+          dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        }
+      }
+    }
+    return dp[a.length][b.length];
+}
+ */
