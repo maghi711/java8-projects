5bf5d9f052b0d577b48b97b8bfaf134485d1bf1c
Mahesh J <maghi711@gmail.com>
2020-11-01 21:01:37 +0530

    Fill Lower Triangular Matrix partial  - 4th problem
    Signed-off-by: Mahesh J <maghi711@gmail.com>

--- src/main/java/com/aadavan/dsa/dp2/pre/_4_FillLowerTriangularMatrix.java
+++ src/main/java/com/aadavan/dsa/dp2/pre/_4_FillLowerTriangularMatrix.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class _3_FillUpperTriangularMatrix {
+public class _4_FillLowerTriangularMatrix {
     public static void main(String[] args) {
         List<List<Integer>> list = getList();
-        fillUpperTriangularMatrix(list);
+        fillLowerTriangularMatrix(list);
         System.out.println("list = " + list);
 
     }
 
-    static void fillUpperTriangularMatrix(List<List<Integer>> list) {
+    static void fillLowerTriangularMatrix(List<List<Integer>> list) {
         int n = list.size();
-        for (int i = n-2; i >= 0; i--) {
-            //System.out.println(i + " " + list.get(i).get(i));
-            for (int j = 0; j <= n-1; j++) {
+        for (int i = 2; i < 3; i++) {
+            System.out.println(i + " " + list.get(i).get(i));
+            for (int j = n-1; j <= 0; j--) {
                 if (j >= i) {
                     continue;
                 }
-                list.get(i).set(j, (list.get(i+1).get(j) + list.get(i).get(j-1)));
-                //System.out.println(list.get(i).get(j));
+                list.get(i).set(j, (list.get(i-1).get(j) + list.get(i).get(j+1)));
+                System.out.println(list.get(i).get(j));
             }
-            //System.out.println();
+            System.out.println();
         }
     }
 
