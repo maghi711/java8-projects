a53999b6f618fee0b48a694a4c904b7b2fe8f500
Mahesh J <maghi711@gmail.com>
2020-11-06 07:50:11 +0530

    Fill Matrix Spirally  - 7th problem
    Signed-off-by: Mahesh J <maghi711@gmail.com>

---
+++ src/main/java/com/aadavan/dsa/dp2/pre/_7_PrintMatrixSpirally.java
@@ -0,0 +1,62 @@
+package com.aadavan.dsa.dp2.pre;
+
+import java.util.Arrays;
+
+public class _7_PrintMatrixSpirally {
+
+    public static void main(String[] args) {
+        int[][] a = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        };
+        printMatrixArray(a);
+        fillSpirally(a);
+        printMatrixArray(a);
+    }
+
+    private static void printMatrixArray(int[][] a) {
+        for (int[] ab: a) {
+            System.out.print(Arrays.toString(ab));
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void fillSpirally(int[][] a) {
+        int m = a.length;
+        int n = a[0].length;
+        int k = 0;
+        int l = 0;
+        int i;
+        int number = 2;
+        while (k < m && l < n) {
+            for (i = l; i < n; ++i) {
+                    a[k][i] = number++;
+            }
+            k++;
+            // Print the last column from the remaining
+            // columns
+            for (i = k; i < m; ++i) {
+                a[i][n - 1] = number++;
+            }
+            n--;
+            // Print the last row from the remaining rows */
+            if (k < m) {
+                for (i = n - 1; i >= l; --i) {
+                    a[m - 1][i] = number++;
+                }
+                m--;
+            }
+
+            // Print the first column from the remaining
+            // columns */
+            if (l < n) {
+                for (i = m - 1; i >= k; --i) {
+                    a[i][l] = number++;
+                }
+                l++;
+            }
+        }
+    }
+}
