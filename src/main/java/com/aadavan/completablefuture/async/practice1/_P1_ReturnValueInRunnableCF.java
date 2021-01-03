2e8c3067628a07b35ca2567dcb1556a0077b9cc7
Mahesh J <maghi711@gmail.com>
2021-01-03 21:58:45 +0530

    Return a result in a runnable using completable future.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

---
+++ src/main/java/com/aadavan/completablefuture/async/practice1/_P1_ReturnValueInRunnableCF.java
@@ -0,0 +1,20 @@
+package com.aadavan.completablefuture.async.practice1;
+
+import com.aadavan.completablefuture.utils.ThreadUtils;
+
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class _P1_ReturnValueInRunnableCF {
+    public static void main(String[] args) {
+        CompletableFuture<String> cf = new CompletableFuture<>();
+        Runnable task = () -> {
+            ThreadUtils.delay(100, TimeUnit.MILLISECONDS);
+            cf.complete("Aadavan the time is " + new Date());
+        };
+        CompletableFuture.runAsync(task);
+        String result = cf.join();
+        System.out.println(result);
+    }
+}
