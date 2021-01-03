66e31f43c269aad0d261c152cf48e3db83503cad
Mahesh J <maghi711@gmail.com>
2021-01-03 21:37:48 +0530

    Supplier launched in two ways using CF. One is without executor service and one is with executor service.
    Signed-off-by: Mahesh J <maghi711@gmail.com>

--- src/main/java/com/aadavan/completablefuture/async/practice1/_P1_SupplierCF.java
+++ src/main/java/com/aadavan/completablefuture/async/practice1/_P1_SupplierCF.java
@@ -3,13 +3,38 @@ package com.aadavan.completablefuture.async.practice1;
 import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
 public class _P1_SupplierCF {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+    public static void main(String[] args) {
+        firstWay();
+        secondWay();
+    }
+
+    private static void firstWay() {
+        Supplier<String> supplier = () -> Thread.currentThread().getName();
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(supplier);
+        String join = stringCompletableFuture.join();
+        System.out.println(join);
+    }
+
+    private static void secondWay() {
+        Supplier<String> supplier = () -> Thread.currentThread().getName();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        CompletableFuture<String> scf = CompletableFuture.supplyAsync(supplier, executor);
+        String join = scf.join();
+        System.out.println(join);
+        executor.shutdown();
+    }
+
+    public static void main_(String[] args) throws InterruptedException, ExecutionException {
         Supplier<String> supplierTask = () -> "Hello Aadavan, the date and time now is " + new Date();
         CompletableFuture<String> cf = CompletableFuture.supplyAsync(supplierTask);
         String s = cf.get();
         System.out.println(s);
     }
+
 }
