9a3b50ba3c8e27f149a562b5314d3867e38fe314
Mahesh J <mahesh.j@wibmo.com>
2021-02-24 12:58:17 +0530

    Generate numbers using stream and filter and print them.

--- src/main/java/com/aadavan/streams/learning/NumberIterator.java
+++ src/main/java/com/aadavan/streams/learning/NumberIterator.java
@@ -4,12 +4,42 @@ import java.util.stream.Stream;
 
 public class NumberIterator {
     public static void main(String[] args) {
+        fourthStream();
+    }
+
+    static void firstStream() {
         Stream.iterate(0, n -> n + 1)
-                .limit(100)
-                .filter(x -> x%2==0)
-                .forEach(x -> {
-                    System.out.print(x);
-                    System.out.println();
-                });
+            .limit(100)
+            .filter(x -> x%2==0)
+            .forEach(x -> {
+                System.out.print(x);
+                System.out.println();
+            });
+    }
+    static void secondStreamForIteration() {
+        Stream.iterate(0, n -> n + 1)
+            .limit(10)
+            .filter(x -> x % 2 == 0)
+            .forEach(
+                s -> {
+                    System.out.println(s);
+                }
+            );
+    }
+    static void thirdStream() {
+        Stream.iterate(0, n -> n + 2)
+                .limit(50)
+                .filter(n -> n %2 == 0)
+                .forEach(
+                        a -> {
+                            System.out.println(a);
+                        }
+                );
+    }
+    static void fourthStream() {
+        Stream.iterate(0, n -> n + 1)
+                .limit(20)
+                .filter(x -> x %2 == 0)
+                .forEach(s -> System.out.println(s));
     }
 }
