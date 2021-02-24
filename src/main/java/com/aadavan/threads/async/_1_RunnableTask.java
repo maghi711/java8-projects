c26558dc036221b2cf706d2e4a471f07993128c5
Mahesh J <mahesh.j@wibmo.com>
2021-02-24 13:06:39 +0530

    Uses all the pools for execution.

--- src/main/java/com/aadavan/threads/async/_1_RunnableTask.java
+++ src/main/java/com/aadavan/threads/async/_1_RunnableTask.java
@@ -4,11 +4,38 @@ import java.util.concurrent.*;
 
 public class _1_RunnableTask {
     public static void main(String[] args) {
+        singleThreadExecutor();
+        cachedThreadPool();
+        fixedThreadPool();
+    }
+    static void singleThreadExecutor() {
         Runnable task = () -> {
-            System.out.println("Runnable task --> " + Thread.currentThread().getName());
+            System.out.println("Runnable task in singleThreadExecutor --> " + Thread.currentThread().getName());
         };
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(task);
         executorService.shutdown();
+    }
+
+    static void cachedThreadPool() {
+        Runnable task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("completed the execution in cachedThreadPool " + Thread.currentThread().getName());
+        };
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(task);
+        service.shutdown();
+    }
+
+    static void fixedThreadPool() {
+        final ExecutorService service = Executors.newFixedThreadPool(2);
+        service.submit(() -> {
+            System.out.println("Completed the fixed thread pool task. " + Thread.currentThread().getName());
+        });
+        service.shutdown();
     }
 }
