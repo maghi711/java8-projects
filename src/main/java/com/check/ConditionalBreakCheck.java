aec68d391ad622bdef73109e323bd54ba6d645d3
Mahesh J <mahesh.j@wibmo.com>
2021-05-27 22:12:32 +0530

    local changes

--- src/main/java/com/check/ConditionalBreakCheck.java
+++ src/main/java/com/check/ConditionalBreakCheck.java
@@ -1,2 +1,23 @@
-package com.check;public class ConditionalBreakCheck {
+package com.check;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConditionalBreakCheck {
+
+    public static void main(String[] args) {
+        final List<String> names = Arrays.asList("Swetha", "Mahesh", "Aadavan");
+        System.out.println("Checking exceptional check");
+        for (String name: names) {
+            System.out.println("Working on " + name);
+            if ("Swetha".equalsIgnoreCase(name)) {
+                if (true) {
+                    break;
+                }
+            }
+            System.out.println("Finished for loop");
+        }
+        //names.stream().filter();
+        //names.stream().anyMatch();
+    }
 }
