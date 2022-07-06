0601f0d04da88fccedb0867ce7a29c0ba0e1ac4b
Mahesh J <maghi711@gmail.com>
2022-07-06 05:42:47 +0530

    added runnable and supplier examples, also fixed logger warning

---
+++ src/main/java/com/aadavan/completablefuture/async/A1FIrstRunnableCF.java
@@ -0,0 +1,46 @@
+package com.aadavan.completablefuture.async;
+
+import com.aadavan.completablefuture.utils.ThreadUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class A1FIrstRunnableCF {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(A1FIrstRunnableCF.class);
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        practice();
+    }
+
+    public static void practice() {
+        executeRunnable();
+        executeSupplier();
+    }
+
+    private static void executeSupplier() {
+        Supplier<String> supplier = () -> {
+            LOGGER.info(getCurrentTime());
+            return "Aadavan";
+        };
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(supplier);
+        String result = stringCompletableFuture.join();
+        LOGGER.info("My name is {}", result);
+    }
+
+    private static void executeRunnable() {
+        Runnable task = () -> LOGGER.info(getCurrentTime());
+        // Runs in Fork Join Pool
+        CompletableFuture.runAsync(task);
+        ThreadUtils.delay(2, TimeUnit.SECONDS);
+    }
+
+    private static String getCurrentTime() {
+        return "The current time is --> " + new Date();
+    }
+}
--- src/main/java/com/aadavan/completablefuture/utils/ThreadUtils.java
+++ src/main/java/com/aadavan/completablefuture/utils/ThreadUtils.java
@@ -2,18 +2,21 @@ package com.aadavan.completablefuture.utils;
 
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.*;
+
 public class ThreadUtils {
 
     public static void delay(long seconds, TimeUnit t) {
         try {
             t.sleep(seconds);
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
         // Usage: use the delay api as defined below
-        ThreadUtils.delay(1, TimeUnit.SECONDS);
+        ThreadUtils.delay(1, SECONDS);
     }
 }
